8a0542c3809194766fcb56e8490d9cf5cd74f873
raveliux <80916575+raveliux@users.noreply.github.com>
2022-08-12 23:38:14 -0500

    Actualización Subescena 1

--- 1/PEyBO_E1.py
+++ 1/PEyBO_E1.py
@@ -14,7 +14,6 @@ from alganim import DashedArrow
 ###############################  versión: Manim Community v0.8.0   ##################
 #####################################################################################
 
-# Constantes de los colores usados.
 ROJO = '#FF0000'
 AZUL = '#0087FF'
 NARANJA = '#FF7700'
@@ -36,6 +35,96 @@ Esta primera escena consta de tres partes:
     Escena geométrica
     Planteamiento del problema en general 
 '''
+
+
+class DashedArrow(DashedLine):
+    """An arrow.
+    Parameters
+    ----------
+    args : Any
+        Arguments to be passed to :class:`Line`.
+    stroke_width : :class:`float`, optional
+        The thickness of the arrow. Influenced by :attr:`max_stroke_width_to_length_ratio`.
+    buff : :class:`float`, optional
+        The distance of the arrow from its start and end points.
+    max_tip_length_to_length_ratio : :class:`float`, optional
+        :attr:`tip_length` scales with the length of the arrow. Increasing this ratio raises the max value of :attr:`tip_length`.
+    max_stroke_width_to_length_ratio : :class:`float`, optional
+        :attr:`stroke_width` scales with the length of the arrow. Increasing this ratio ratios the max value of :attr:`stroke_width`.
+    kwargs : Any
+        Additional arguments to be passed to :class:`Line`.
+    """
+    def __init__(
+        self,
+        *args,
+        stroke_width=6,
+        buff=MED_SMALL_BUFF,
+        max_tip_length_to_length_ratio=0.25,
+        max_stroke_width_to_length_ratio=5,
+        **kwargs,
+    ):
+        self.max_tip_length_to_length_ratio = max_tip_length_to_length_ratio
+        self.max_stroke_width_to_length_ratio = max_stroke_width_to_length_ratio
+        tip_shape = kwargs.pop("tip_shape", ArrowTriangleFilledTip)
+        super().__init__(*args, buff=buff, stroke_width=stroke_width, **kwargs)
+        # TODO, should this be affected when
+        # Arrow.set_stroke is called?
+        self.initial_stroke_width = self.stroke_width
+        self.add_tip(tip_shape=tip_shape)
+        self.set_stroke_width_from_length()
+    def scale(self, factor, scale_tips=False, **kwargs):
+        if self.get_length() == 0:
+            return self
+
+        if scale_tips:
+            super().scale(factor, **kwargs)
+            self.set_stroke_width_from_length()
+            return self
+
+        has_tip = self.has_tip()
+        has_start_tip = self.has_start_tip()
+        if has_tip or has_start_tip:
+            old_tips = self.pop_tips()
+
+        super().scale(factor, **kwargs)
+        self.set_stroke_width_from_length()
+
+        if has_tip:
+            self.add_tip(tip=old_tips[0])
+        if has_start_tip:
+            self.add_tip(tip=old_tips[1], at_start=True)
+        return self
+
+    def get_normal_vector(self) -> np.ndarray:
+        p0, p1, p2 = self.tip.get_start_anchors()[:3]
+        return normalize(np.cross(p2 - p1, p1 - p0))
+
+    def reset_normal_vector(self):
+        """Resets the normal of a vector"""
+        self.normal_vector = self.get_normal_vector()
+        return self
+
+    def get_default_tip_length(self) -> float:
+        """Returns the default tip_length of the arrow.
+        """
+        max_ratio = self.max_tip_length_to_length_ratio
+        return min(self.tip_length, max_ratio * self.get_length())
+
+    def set_stroke_width_from_length(self):
+        """Used internally. Sets stroke width based on length."""
+        max_ratio = self.max_stroke_width_to_length_ratio
+        if config.renderer == "opengl":
+            self.set_stroke(
+                width=min(self.initial_stroke_width, max_ratio * self.get_length()),
+                recurse=False,
+            )
+        else:
+            self.set_stroke(
+                width=min(self.initial_stroke_width, max_ratio * self.get_length()),
+                family=False,
+            )
+        return self
+
 
 
 class Subescena_1(Scene):
@@ -95,8 +184,8 @@ class Subescena_1(Scene):
         self.play(Write(group_1[2][:]),
                   run_time=5
                   )
-        self.wait(3)
-        self.wait(11)
+        self.wait(2)
+        self.wait(5)
         self.play(
             group_1[2][:].animate.set_color(AZUL),
             run_time=1
@@ -107,7 +196,7 @@ class Subescena_1(Scene):
         )
         self.play(
             group_1[0][:].animate.set_color(AZUL),
-            run_time=2
+            run_time=1
         )
         self.play(
             group_1[0][:].animate.set_color(WHITE),
@@ -123,7 +212,7 @@ class Subescena_1(Scene):
             Transform(group_1[3][2], coeficientes_ci[2]),
             run_time=2
         )
-        self.wait(2)
+        self.wait(1)
         self.play(
             *[FadeOut(mob) for mob in self.mobjects],
             run_time=1)
