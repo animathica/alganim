5400459069c95f1366230ab2ebfbb262c4398c43
dabnciencias <dabn@ciencias.unam.mx>
2023-07-15 00:26:43 -0600

    Terminar animaciones de V2E1, V2E2 y V2E4.

--- 2/NIyBO_E3.py
+++ 2/NIyBO_E3.py
@@ -23,27 +23,81 @@ TEAL_E = "#49A88F"
 MOSTAZA_OSCURO = "#FFD025"
 MOSTAZA_CLARO = "#FFE072"
 
-class E3(Scene):
+class E3(MovingCameraScene):
     def construct(self):
 
-       texto1 = MathTex(r"(V,K)").shift(3*LEFT+ 2*UP).scale(0.6)
-       texto1.to_edge(RIGHT)
-       texto2 = MathTex(r" \langle \cdot , \cdot \rangle : V\times V\to K \ \text{es un producto escalar en} \ V ").scale(.6)
+        texto1 = MathTex(r"(V,K)").scale(0.6)
+        texto2 = MathTex(r" \langle \cdot , \cdot \rangle : V\times V\to K \ \text{es un producto escalar en} \ V ").scale(.6)
 
-       texto_1=VGroup(texto1, texto2).arrange(direction=DOWN, center=False, aligned_edge=RIGHT)
+        texto_1=VGroup(texto1, texto2).arrange(direction=1.5*DOWN).shift(2*UP)
 
-       texto3=MathTex(r"||\vec{v}||:=\sqrt{\langle \vec{v} , \vec{v} \rangle} \quad \forall \ \vec{v}\in V").scale(.6).shift(0.5*UP)
-       texto4=MathTex(r"||\cdot||:V \to K").scale(.6)
-       texto5=MathTex(r"\forall \ \vec{v}\in V,", r" \forall \ a \in K,").scale(.6)
-       texto6=MathTex(r"||a\vec{u}|| = \sqrt{\langle a\vec{u} , a\vec{u} \rangle}", r"= \sqrt{ a\overline{a}\langle \vec{u} , \vec{u} \rangle}", r" = \sqrt{a\overline{a}}", r"\sqrt{\langle \vec{u} , \vec{u} \rangle}", r" = |a| \ ||\vec{u}||").scale(.6)
-       texto7=MathTex(r"|| \vec{u} ||= 0", r" \Longleftrightarrow" r"\sqrt{\langle \vec{u} , \vec{u} \rangle}", r" = 0", r"\Longleftrightarrow", r" \langle \vec{u} , \vec{u} \rangle = 0", r" \Longleftrightarrow ",  r"\vec{u} = \vec{0}").scale(.6)
+        texto3=MathTex(r"||\vec{v}||:=\sqrt{\langle \vec{v} , \vec{v} \rangle} \quad \forall \ \vec{v}\in V").scale(.6)
+        texto4=MathTex(r"||\cdot||:V \to \mathbb{R}^{\geq0}").scale(.6)
+        texto5=MathTex(r"\forall \ \vec{u}\in V,", r" \forall \ a \in K,").scale(.6)
 
-       texto_2=VGroup(texto3, texto4, texto5,  texto6, texto7).arrange(direction=DOWN, center=False, aligned_edge=RIGHT)
+        texto_2=VGroup(texto3, texto4, texto5).arrange(direction=1.5*DOWN, center=True).next_to(texto_1, 1.5*DOWN)
 
-       self.wait()
+        texto6=MathTex(r"||a\vec{u}|| = \sqrt{\langle a\vec{u} , a\vec{u} \rangle}", r"= \sqrt{ a\overline{a}\langle \vec{u} , \vec{u} \rangle}", r" = \sqrt{a\overline{a}}", r"\sqrt{\langle \vec{u} , \vec{u} \rangle}", r" = |a| \ ||\vec{u}||").scale(.6)
+        texto7=MathTex(r"|| \vec{u} ||= 0", r" \Longleftrightarrow" r"\sqrt{\langle \vec{u} , \vec{u} \rangle}", r" = 0", r"\Longleftrightarrow", r" \langle \vec{u} , \vec{u} \rangle = 0", r" \Longleftrightarrow ",  r"\vec{u} = \vec{0}").scale(.6)
 
-       for element in texto_1:
-         self.play(Write(element), run_time=2)
+        texto_3=VGroup(texto6, texto7).arrange(direction=1.5*DOWN, center=False, aligned_edge=RIGHT).next_to(texto_2, 1.5*DOWN)
+        texto_3.shift(2.5*LEFT)
 
-       for element in texto_2:
-         self.play(Write(element), run_time=2)
+        texto8=Tex("Escalabilidad absoluta").scale(.6)
+        texto9=Tex("Distinción del vector nulo").scale(.6)
+        texto10=Tex("Desigualdad del triángulo").scale(.6)
+
+        texto11=MathTex(r"||\cdot||:V \to \mathbb{R}^{\geq0} \text{ es una \emph{norma inducida por un producto escalar}").scale(.6).shift(3.25*DOWN)
+
+        texto_4=VGroup(texto8, texto9, texto10).arrange(direction=1.9*DOWN, center=False, aligned_edge=LEFT).next_to(texto_2, 1.5*DOWN).shift(3.5*RIGHT+0.125*DOWN)
+
+        p1 = Line(ORIGIN, [1,1,0], color = VERDE, buff = 2).shift(0.1*LEFT)
+        p2 = Line([-0.5,0.5,0], ORIGIN, color = VERDE, buff = 2)
+        
+        paloma1 = VGroup(p1,p2).scale(0.2).set_opacity(0).shift(5.5*RIGHT+1.65*DOWN)
+        paloma2 = paloma1.copy().shift(0.7*DOWN)
+        paloma3 = paloma1.copy().shift(1.4*DOWN)
+
+        #Animaciones
+
+        self.camera.frame.shift(0.4*DOWN)
+
+        self.play(Write(texto1, run_time=0.5))
+        self.play(Write(texto2))
+
+        self.wait()
+
+        self.play(Write(texto3))
+
+        self.wait(3)
+
+        self.play(Write(texto4))
+
+        self.wait()
+
+        texto5.shift(2.5*LEFT)
+        self.play(Write(texto5))
+
+        self.play(Write(texto6))
+
+        self.play(Write(texto7))
+
+        self.wait()
+
+        self.play(Write(texto8))
+
+        self.play(Write(texto9))
+
+        self.add(paloma1, paloma2, paloma3)
+        self.play(paloma1.animate.set_opacity(1),
+                  paloma2.animate.set_opacity(1))
+
+        self.play(Write(texto10))
+
+        self.play(paloma3.animate.set_opacity(1))
+
+        self.play(Write(texto11[0][0:21]))
+
+        self.wait(7)
+
+        self.play(Write(texto11[0][21:]))
--- 2/NIyBO_E5.hs
+++ 2/NIyBO_E5.hs
@@ -17,19 +17,17 @@ import Reanimate.Scene
 main :: IO ()
 main = reanimate $ addStatic (mkBackground "black") $ scene $ do
 
-  -- Este bloque dibuja la cabecera, asegurándose de que esté centrada; si no es necesario, se puede "comentar" con "--".
+  -- Este bloque genera una lista con los objetos de texto (SVGs) que serán utilizados en la primera parte de la escena.
   
---  cab <- oNew c1l1
---  oModify cab $ oCenterX .~ 0
---  oModify cab $ oTopY .~ 4.25
---  oShowWith cab $ adjustDuration (*0.33) . oDraw
---  wait 0.5
-  
-  -- Este bloque genera dos listas con los demás objetos de texto (SVGs) que serán utilizados en la escena.
-  
-  texts1 <- mapM oNew [ e1p0, e1p1, e2p0, e2p1, e3p0, e3p1 ]
+  texts1 <- mapM oNew [ e1p0, e1p1, e1p2, e1p3, e1p4, e1p5, e2p0, e2p1 ]
 
---  texts2 <- mapM oNew [ e3p0, e3p1, e3p2, e4p0, e4p1, e5p0, e5p1 ]
+  -- Este bloque genera una lista con los objetos de texto (SVGs) que serán utilizados en la segunda parte de la escena.
+
+  texts2 <- mapM oNew [ c1p0, e3p0, e3p1, e3p2, e3p3, e4p0, e4p1, e4p2, e4p3 ]
+
+  -- Este bloque genera una lista con los objetos de texto (SVGs) que serán utilizados en la tercera parte de la escena.
+
+  texts3 <- mapM oNew [ p5p0, p5p1, p5p2, p5p3, p5p4 ]
 
   -- Este bloque describe lo que sucederá en la escena con los demás objetos de texto.
 
@@ -44,46 +42,70 @@ main = reanimate $ addStatic (mkBackground "black") $ scene $ do
     \obj -> fork $ do
     oHideWith obj oFadeOut
 
---  wait 1
---
---  forM_ (zip5 texts2 leftXs2 topYs2 durationFunctions2 waitDurations2) $    -- Creamos una lista de 5-tuplas a partir de las
---    \(obj, xPos, yPos, dFunc, wDur) -> do                              -- listas de parámetros;
---    oModify obj $ oLeftX .~ xPos                                       -- modificamos la posición horizontal del objeto;
---    oModify obj $ oTopY .~ (yPos - 0.5)                                        -- modificamos la posición vertical del objeto;
---    oShowWith obj $ adjustDuration dFunc . oDraw                       -- dibujamos el objeto ajustando la velocidad;
---    wait wDur                                                          -- esperamos una cantidad indicada de tiempo.
---
---  forM_ texts2 $
---    \obj -> fork $ do
---    oHideWith obj oFadeOut
+  wait 1
+
+  forM_ (zip5 texts2 leftXs2 topYs2 durationFunctions2 waitDurations2) $    -- Creamos una lista de 5-tuplas a partir de las
+    \(obj, xPos, yPos, dFunc, wDur) -> do                              -- listas de parámetros;
+    oModify obj $ oLeftX .~ xPos                                       -- modificamos la posición horizontal del objeto;
+    oModify obj $ oTopY .~ (yPos - 0.5)                                        -- modificamos la posición vertical del objeto;
+    oShowWith obj $ adjustDuration dFunc . oDraw                       -- dibujamos el objeto ajustando la velocidad;
+    wait wDur                                                          -- esperamos una cantidad indicada de tiempo.
+
+  forM_ texts2 $
+    \obj -> fork $ do
+    oHideWith obj oFadeOut
+    
+  forM_ (zip5 texts3 leftXs3 topYs3 durationFunctions3 waitDurations3) $    -- Creamos una lista de 5-tuplas a partir de las
+    \(obj, xPos, yPos, dFunc, wDur) -> do                              -- listas de parámetros;
+    oModify obj $ oLeftX .~ xPos                                       -- modificamos la posición horizontal del objeto;
+    oModify obj $ oTopY .~ (yPos - 0.5)                                        -- modificamos la posición vertical del objeto;
+    oShowWith obj $ adjustDuration dFunc . oDraw                       -- dibujamos el objeto ajustando la velocidad;
+    wait wDur                                                          -- esperamos una cantidad indicada de tiempo.
+
+  forM_ texts3 $
+    \obj -> fork $ do
+    oHideWith obj oFadeOut
+    
 
 -----------------------------------------------------------------------------
 -- Listas de parámetros utilizados en la escena para cada objeto de texto. --
 -----------------------------------------------------------------------------
 
 leftXs1 :: [Double]
-leftXs1 = [-7.75, -5.5, -2.75, -7.75, -7.75, -5.4, -7.75, -7.75, -7.75, -7.75, -7.75]
+leftXs1 = [-7.75, -5.5, -7.75, -2.25, -7.75, -7.75, -7.75, -7.75]
 
 topYs1 :: [Double]
-topYs1 = [3.5, 3.5, 3, 1.5, 0, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
+topYs1 = [3.5, 3.525, 3.5, 2.5, 1.5, 1.5, 0, 0]
 
 durationFunctions1 :: [(Duration -> Duration)]
-durationFunctions1 = [(*0.3), (*0.33), (*0.5), (*0.5), (*0.5), (*0.5), (*0.5), (*0.5), (*0.5), (*0.5), (*0.5)]
+durationFunctions1 = [(*0.3), (*0.33), (*0.4), (*0.75), (*0.5), (*0.33), (*0.3), (*0.33)]
 
 waitDurations1 :: [Double]
-waitDurations1 = [0.5, 0, 0.25, 0, 0, 0, 0, 0, 0, 0, 0]
+waitDurations1 = [0.5, 0.25, 0, 0, 0, 0.5, 0.5, 2.5]
 
 leftXs2 :: [Double]
-leftXs2 = [-7.75, -5.5, -7.75, -7.75, -7.75, -7.75, -7.75]
+leftXs2 = [-7.25, -7.75, -5.5, -7.75, -7.75, -7.75, -7.75, -2.25, -7.75]
 
 topYs2 :: [Double]
-topYs2 = [3.5, 3.5, 2.5, -0.5, -0.5, -2, -2]
+topYs2 = [4.75, 3.25, 3.25, 2.25, 1.5, -0.25, -0.25, -1.25, -2.75]
 
 durationFunctions2 :: [(Duration -> Duration)]
-durationFunctions2 = [(*0.3), (*0.33), (*0.3), (*0.33), (*0.33), (*0.33), (*0.33)]
+durationFunctions2 = [(*0.4), (*0.3), (*0.3), (*0.33), (*0.5), (*0.3), (*0.33), (*0.5), (*0.4)]
 
 waitDurations2 :: [Double]
-waitDurations2 = [0.5, 0, 0.5, 2, 0, 0, 0]
+waitDurations2 = [0.5, 0.5, 0, 0.5, 2, 0, 0, 0, 2.5]
+
+leftXs3 :: [Double]
+leftXs3 = [-7.75, -7.75, -7.75, -7.75, -4]
+
+topYs3 :: [Double]
+topYs3 = [3.25, 3.25, 2.3, 1.85, -0.175]
+
+durationFunctions3 :: [(Duration -> Duration)]
+durationFunctions3 = [(*0.3), (*0.33), (*0.33), (*0.33), (*0.33)]
+
+waitDurations3 :: [Double]
+waitDurations3 = [0.5, 0.25, 0.5, 0, 0]
 
 ------------------------------------------------------------------------------------------------------------------
 -- Aquí va el texto que escribiremos, separado en pedazos; usamos "\\hfill\\break" para romper líneas en LaTeX. --
@@ -92,43 +114,44 @@ waitDurations2 = [0.5, 0, 0.5, 2, 0, 0, 0]
 ejercicio1 :: SVG    -- Definimos el SVG del primer ejercicio con los atributos deseados.
 ejercicio1 = withSubglyphs [0..11] (withStrokeColorPixel miAzul) $ withSubglyphs [0..11] (withFillColorPixel miAzul) $
             withStrokeWidth 0 $ withFillOpacity 1 $ withStrokeColor "white" $ withFillColor "white" $ scale 0.4 $ 
-                latexCfg myTexConfig "Ejercicio 2.1 Sean $V$ un espacio normado y $\\vec{u}\\in V$. Demuestra que $||\\vec{u}||>0$ si $\\vec{u}\\neq\\vec{0}$, es decir, que la norma es positivo definida."
+                latexCfg myTexConfig "Ejercicio 2.1 Sea $V$ un espacio vectorial con norma $||\\cdot||:V\\to \\mathbb{R}^{\\ge0}$. Para $\\vec{u}\\in V$, demuestra que $$\\vec{u}\\neq\\vec{0} \\implies ||\\vec{u}||>0,$$ es decir, que la norma es positivo definida. Adicionalmente, demuestra que la implicación contraria también se verifica."
 
 e1p0 :: SVG
 e1p0 = split [0..11] ejercicio1
 
 e1p1 :: SVG
-e1p1 = split [12..23] ejercicio1
+e1p1 = split [12..53] ejercicio1
 
 e1p2 :: SVG
-e1p2 = split [24..81] ejercicio1
+e1p2 = split [54..74] ejercicio1
 
 e1p3 :: SVG
-e1p3 = split [82..200] ejercicio1
+e1p3 = split [75..91] ejercicio1
 
 e1p4 :: SVG
-e1p4 = split [82..200] ejercicio1
+e1p4 = split [92..127] ejercicio1
 
 e1p5 :: SVG
-e1p5 = split [82..200] ejercicio1
-
-e1p6 :: SVG
-e1p6 = split [82..200] ejercicio1
+e1p5 = split [128..200] ejercicio1
 
 ejercicio2 = withSubglyphs [0..11] (withStrokeColorPixel miAzul) $ withSubglyphs [0..11] (withFillColorPixel miAzul) $
             withStrokeWidth 0 $ withFillOpacity 1 $ withStrokeColor "white" $ withFillColor "white" $ scale 0.4 $ 
-                latexCfg myTexConfig "Ejercicio 2.2 Una \\emph{métrica} o \\emph{función de distancia} en un conjunto $X$ es una función $f(\\cdot,\\cdot):X\\times X\\to [0,\\infty)$ que cumple las siguientes propiedades: \\hfill \\break (i) $d(x,y) = 0$ si, y sólo si, $x=y$; \\hfill \\break \\break (ii) $d(x,y) = d(y,x)$ para cualesquiera $x,y\\in X$; \\hfill \\break \\break (iii) $d(x,y) \\leq d(x,z) + d(z,y)$ para cualesquiera $x,y,z\\in X$. \\hfill \\break Demuestra que si $(V,K)$ es un espacio vectorial con norma $||\\cdot||:V\\to\\mathbb{R}$, entonces la función dada por $$d(\\vec{x},\\vec{y}) = ||\\vec{x}-\\vec{y}|| \\quad \\forall \\ \\vec{x}, \\vec{y}\\in V$$ es una métrica en $V$. Por ende, toda norma \\emph{induce} una métrica y, en particular, todo producto escalar positivo definido induce una métrica; sin embargo, existen métricas que no son inducidas por normas ni productos escalares."
+                latexCfg myTexConfig "Ejercicio 2.2 Demuestra que todo conjunto ortonormal finito es linealmente independiente."
 
 e2p0 :: SVG
 e2p0 = split [0..11] ejercicio2
 
 e2p1 :: SVG
-e2p1 = split [12..100] ejercicio2
+e2p1 = split [12..78] ejercicio2
+
+c1p0 :: SVG      -- Definimos el SVG de la cabecera con los atributos deseados.
+c1p0 = withStrokeWidth 0 $ withFillOpacity 1 $ withStrokeColor "white" $ withFillColor "white" $ center $ scale 0.4 $
+         latex "Sean $(V,K)$ un espacio vectorial con producto escalar $\\langle \\cdot , \\cdot \\rangle:V\\times V\\to K$, $\\text{dim}(V)<\\infty$ y $N=\\{\\hat{n}_1, \\hat{n}_2, \\dots, \\hat{n}_k\\}\\subseteq V$ considerando la norma inducida."
 
 ejercicio3 :: SVG    -- Análogo a lo anterior.
 ejercicio3 = withSubglyphs [0..11] (withStrokeColorPixel miAzul) $ withSubglyphs [0..11] (withFillColorPixel miAzul) $
             withStrokeWidth 0 $ withFillOpacity 1 $ withStrokeColor "white" $ withFillColor "white" $ scale 0.4 $ 
-                latex "Ejercicio 2.3 Demuestra que todo conjunto ortonormal finito es linealmente independiente."
+                latex "Ejercicio 2.3 Demuestra que las siguientes condiciones son equivalentes. \\hfill \\break \\break (a) $N$ es una base ortonormal de $V$. \\hfill \\break \\break (b) $\\langle N \\rangle = V$ y $\\langle \\hat{n}_i, \\hat{n}_j \\rangle = \\begin{cases} 1 \\ \\ \\text{si} \\ j=i, \\\\ 0 \\ \\ \\text{si} \\ j\\neq i. \\end{cases}$"
 
 e3p0 :: SVG
 e3p0 = split [0..11] ejercicio3
@@ -137,18 +160,48 @@ e3p1 :: SVG
 e3p1 = split [12..63] ejercicio3
 
 e3p2 :: SVG
-e3p2 = split [64..122] ejercicio3
+e3p2 = split [64..90] ejercicio3
 
---pregunta4 :: SVG    -- Análogo a lo anterior.
---pregunta4 = withSubglyphs [0..10] (withStrokeColorPixel miRojo) $ withSubglyphs [0..10] (withFillColorPixel miRojo) $
---           withStrokeWidth 0 $ withFillOpacity 1 $ withStrokeColor "white" $ withFillColor "white" $ scale 0.4 $ 
---           latex "Pregunta 2.3 "
---
---p4p0 :: SVG
---p4p0 = split [0..10] pregunta5
---
---p4p1 :: SVG
---p4p1 = split [11..153] pregunta5
+e3p3 :: SVG
+e3p3 = split [91..125] ejercicio3
+
+ejercicio4 :: SVG    -- Análogo a lo anterior.
+ejercicio4 = withSubglyphs [0..11] (withStrokeColorPixel miAzul) $ withSubglyphs [0..11] (withFillColorPixel miAzul) $
+            withStrokeWidth 0 $ withFillOpacity 1 $ withStrokeColor "white" $ withFillColor "white" $ scale 0.4 $ 
+                latex "Ejercicio 2.4 Demuestra que $N=\\{\\hat{n}_1,\\hat{n}_2,\\dots,\\hat{n}_k\\}$ es una base \\emph{ortonormal} de $V$ si, y sólo si, $$\\vec{v} = \\sum_{i=1}^k \\langle\\vec{v}, \\hat{n}_i\\rangle \\hat{n}_i \\quad \\forall \\ \\vec{v}\\in V.$$ Más aún, demuestra que en este caso $\\big \\langle \\langle \\vec{v} , \\hat{n}_i \\rangle \\hat{n}_i, \\langle \\vec{v} , \\hat{n}_j \\rangle \\hat{n}_j \\big \\rangle = 0$ si $i\\neq j$." 
+
+e4p0 :: SVG
+e4p0 = split [0..11] ejercicio4
+
+e4p1 :: SVG
+e4p1 = split [12..76] ejercicio4
+
+e4p2 :: SVG
+e4p2 = split [77..101] ejercicio4
+
+e4p3 :: SVG
+e4p3 = split [102..166] ejercicio4
+e4p4 = split [102..166] ejercicio4
+
+pregunta5 :: SVG    -- Análogo a lo anterior.
+pregunta5 = withSubglyphs [0..10] (withStrokeColorPixel miRojo) $ withSubglyphs [0..10] (withFillColorPixel miRojo) $
+           withStrokeWidth 0 $ withFillOpacity 1 $ withStrokeColor "white" $ withFillColor "white" $ scale 0.4 $ 
+           latexCfg myTexConfig "Pregunta 2.5 Por la propiedad de \\emph{escalabilidad absoluta} de la norma, multiplicar a un vector arbitrario por un escalar con valor absoluto $1$ deja invariante su norma. ¿Cómo se interpreta esto geométricamente en espacios vectoriales reales y complejos? (Sugerencia: Considera vectores de $\\mathbb{R}^2$ multiplicados por escalares de $\\{x\\in \\mathbb{R} \\mid |x| = 1\\}$ y vectores de $\\mathbb{C}$ multiplicados por escalares de $\\{z\\in\\mathbb{C} \\mid |z|=1\\}$. En cada caso, la multiplicación de vectores por escalares de valor absoluto 1 se interpreta como un tipo de operación geométrica bajo la cual la norma es invariante. ¿Cuáles son?)"
+
+p5p0 :: SVG
+p5p0 = split [0..10] pregunta5
+
+p5p1 :: SVG
+p5p1 = split [11..138] pregunta5
+
+p5p2 :: SVG
+p5p2 = split [139..214] pregunta5
+
+p5p3 :: SVG
+p5p3 = split [215..477] pregunta5
+
+p5p4 :: SVG
+p5p4 = split [478..490] pregunta5
 
 --------------------------------------------------------------------------------------------------------------
 -- Funciones auxiliares. En las primeras líneas se declaran las signaturas y en las siguientes, se definen. --
